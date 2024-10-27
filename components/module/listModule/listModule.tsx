410e0b217d70e5cdd2d47b6211cf164f3b02e490
Damyan Myashkov <damyan.myashkov@gmail.com>
2024-10-27 19:22:35 +0200

    At least expansions and add animations work

--- components/module/listModule/listModule.tsx
+++ components/module/listModule/listModule.tsx
@@ -72,6 +72,8 @@ type ActivityProps = {
   onClickAddButton?: () => void;
   addAnim?: SharedValue<number>;
   onFocusAdditional?: () => void;
+  expandAnimProp?: SharedValue<number>;
+  isParentInFocusGroup?: boolean;
 };
 
 export default function Activity({
@@ -84,6 +86,8 @@ export default function Activity({
   onClickAddButton = () => {},
   addAnim = useSharedValue(0),
   onFocusAdditional = () => {},
+  expandAnimProp = useSharedValue(1),
+  isParentInFocusGroup = true,
 }: ActivityProps) {
   const styles = useStyles();
   // const { theme } = useTheme();
@@ -94,34 +98,50 @@ export default function Activity({
   const isFocused = path === focusedPath;
   const isRoot = path === "/root";
 
-  const expandAnim = useSharedValue(0);
-  const shouldShrink = !path.startsWith(focusedPath);
-  // const isExpanded = useSharedValue(isRoot);
-  const [expandedState, setExpandedState] = useState(isRoot);
-
-  useEffect(() => {
-    expandAnim.value = withTiming(shouldShrink ? 0 : 1, { duration: 300 });
-  }, [shouldShrink, expandAnim]);
-
   const focusAnim = useSharedValue(0);
   useEffect(() => {
     focusAnim.value = withTiming(isFocused ? 1 : 0, { duration: 300 });
   }, [isFocused, focusAnim]);
 
+  const localExpandAnim = useSharedValue(isRoot ? 1 : 0);
+
+  const expandAnim = useDerivedValue(() => {
+    return focusAnim.value > 0
+      ? 1
+      : expandAnimProp.value * localExpandAnim.value;
+  });
+
+  const expandAnimChild = useSharedValue(0);
+
+  const childVisibilityAnim = useDerivedValue(() => {
+    return expandAnim.value * expandAnimChild.value;
+  });
+
+  const isInFocusGroup = path.startsWith(focusedPath);
+  const [expandedState, setExpandedState] = useState(isRoot);
+
+  useEffect(() => {
+    localExpandAnim.value = withTiming(isInFocusGroup ? 1 : 0, {
+      duration: 300,
+    });
+  }, [isInFocusGroup, localExpandAnim]);
+
+  useEffect(() => {
+    expandAnimChild.value = withTiming(expandedState ? 1 : 0, {
+      duration: 300,
+    });
+  }, [expandedState, expandAnimChild]);
+
   const animStyles = {
     activityItem: useAnimatedStyle(() => ({
       height: expandAnim.value * interpolate(focusAnim.value, [0, 1], [40, 82]),
-      marginBottom: interpolate(
-        expandAnim.value,
-        [0, 1],
-        [0, styles.activityItem.marginBottom],
-      ),
+      marginBottom: interpolate(expandAnim.value, [0, 1], [0, 2]),
     })),
     listModule: useAnimatedStyle(() => ({
       marginTop: interpolate(
         expandAnim.value,
         [0, 1],
-        [isFirstInList ? 0 : -styles.list.gap / 2, 0],
+        [-styles.list.gap / 2, 0],
       ),
       marginBottom: interpolate(
         expandAnim.value,
@@ -129,19 +149,14 @@ export default function Activity({
         [isLastInList ? 0 : -styles.list.gap / 2, 0],
       ),
     })),
-    childrenContainerExpanded: useAnimatedStyle(() => {
+    childrenContainer: useAnimatedStyle(() => {
       return {
         marginTop: interpolate(
-          expandAnim.value,
-          [0, 1],
-          [0, !isRoot ? styles.childrenContainer.marginTop : 0],
-        ),
-      };
-    }),
-    childrenContainerCollapsed: useAnimatedStyle(() => {
-      return {
-        marginTop: interpolate(
-          expandAnim.value * addAnim.value,
+          interpolate(
+            childVisibilityAnim.value,
+            [0, 1],
+            [addAnim.value * expandAnim.value, childVisibilityAnim.value],
+          ),
           [0, 1],
           [0, !isRoot ? styles.childrenContainer.marginTop : 0],
         ),
@@ -154,15 +169,14 @@ export default function Activity({
         [0, !isRoot ? styles.lineContainer.width : 0],
       ),
     })),
-    lineExpanded: useAnimatedStyle(() => {
-      return {
-        opacity: interpolate(expandAnim.value, [0, 1], [0, !isRoot ? 1 : 0]),
-      };
-    }),
-    lineCollapsed: useAnimatedStyle(() => {
+    line: useAnimatedStyle(() => {
       return {
         opacity: interpolate(
-          expandAnim.value * addAnim.value,
+          interpolate(
+            childVisibilityAnim.value,
+            [0, 1],
+            [addAnim.value * expandAnim.value, childVisibilityAnim.value],
+          ),
           [0, 1],
           [0, !isRoot ? 1 : 0],
         ),
@@ -182,18 +196,22 @@ export default function Activity({
         marginBottom: interpolate(
           addShrinkAnim,
           [0, 1],
-          [0, styles.activityItem.marginBottom],
+          [
+            0,
+            activityData.activities &&
+            activityData.activities.length > 0 &&
+            !expandedState
+              ? // something is wrong here this can be applied better
+                styles.activityItem.marginBottom - styles.list.gap / 2
+              : styles.activityItem.marginBottom,
+          ],
         ),
         marginTop: interpolate(
           addShrinkAnim,
           [0, 1],
           [-styles.childrenContainer.marginTop, 0],
         ),
-        height: interpolate(
-          shouldShrink === false ? addShrinkAnimStrict : addShrinkAnim,
-          [0, 1],
-          [0, 40],
-        ),
+        height: interpolate(addShrinkAnim, [0, 1], [0, 40]),
       };
     }),
   };
@@ -207,7 +225,10 @@ export default function Activity({
   }, []);
 
   return (
-    <Animated.View style={animStyles.listModule}>
+    <Animated.View
+      style={animStyles.listModule}
+      // key={`${expandAnim.value}${activityData.title}`}
+    >
       {level !== 0 && (
         <ActivityItem
           activityName={activityData.title}
@@ -229,44 +250,33 @@ export default function Activity({
         />
       )}
       <Animated.View
-        key={`children-container-${expandedState}`}
-        style={[
-          styles.childrenContainer,
-          expandedState
-            ? animStyles.childrenContainerExpanded
-            : animStyles.childrenContainerCollapsed,
-        ]}
+        style={[styles.childrenContainer, animStyles.childrenContainer]}
       >
         <Animated.View style={[styles.lineContainer, animStyles.lineContainer]}>
-          <Animated.View
-            style={[
-              styles.line,
-              expandedState
-                ? animStyles.lineExpanded
-                : animStyles.lineCollapsed,
-            ]}
-          />
+          <Animated.View style={[styles.line, animStyles.line]} />
         </Animated.View>
         <View style={[styles.list]}>
           <AddItem
             onClickAddButton={onClickAddButton}
             style={animStyles.addItem}
+            key={`${expandedState}${activityData.title}`}
           />
-          {expandedState &&
-            activityData.activities?.map((activity, index, array) => (
-              <Activity
-                key={activity.id}
-                activityData={activity}
-                level={level + 1}
-                isFirstInList={index === 0}
-                isLastInList={index === array.length - 1}
-                path={`${path}/${activity.id}`}
-                addScreen={addScreen}
-                onClickAddButton={onClickAddButton}
-                addAnim={addAnim}
-                onFocusAdditional={onFocusAdditional}
-              />
-            ))}
+          {activityData.activities?.map((activity, index, array) => (
+            <Activity
+              key={activity.id}
+              activityData={activity}
+              level={level + 1}
+              isFirstInList={index === 0}
+              isLastInList={index === array.length - 1}
+              path={`${path}/${activity.id}`}
+              addScreen={addScreen}
+              onClickAddButton={onClickAddButton}
+              addAnim={addAnim}
+              onFocusAdditional={onFocusAdditional}
+              expandAnimProp={childVisibilityAnim}
+              isParentInFocusGroup={isInFocusGroup}
+            />
+          ))}
         </View>
       </Animated.View>
     </Animated.View>
